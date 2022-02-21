f1add13aba121bccdbfc22dff2330202bfec24dd
Elbay Malik <eleo.666@gmail.com>
2022-02-21 20:52:35 +0100

    Create ViewModel Include logging to observe fragment destroy/creation

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+
+    private var score = 0
+    private var currentWordCount = 0
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+    override fun onCleared(){
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel is destroyed")
+    }
+}
