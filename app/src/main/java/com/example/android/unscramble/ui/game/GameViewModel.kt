0c66c849458f97be351ac70a4a59f1c2a5176e1b
Elbay Malik <eleo.666@gmail.com>
2022-02-21 23:32:31 +0100

    Finalize the code and add explanations

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,12 +4,10 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
-
     private var _score = 0
     val score: Int
         get() = _score
+
     private var _currentWordCount = 0
     val currentWordCount: Int
         get() = _currentWordCount
@@ -17,6 +15,10 @@ class GameViewModel : ViewModel() {
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    // List of words used in the game
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
@@ -28,6 +30,9 @@ class GameViewModel : ViewModel() {
         Log.d("GameFragment", "GameViewModel is destroyed")
     }
 
+    /*
+    * Updates currentWord and currentScrambledWord with the next word.
+    */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
@@ -44,10 +49,17 @@ class GameViewModel : ViewModel() {
         }
     }
 
+    /*
+    * Increases the game score if the player's word is correct.
+    */
     private fun increaseScore() {
         _score += SCORE_INCREASE
     }
 
+    /*
+    * Returns true if the player word is correct.
+    * Increases the score accordingly.
+    */
     fun isUserWordCorrect(playerWord: String): Boolean {
         if (playerWord.equals(currentWord, true)) {
             increaseScore()
@@ -55,7 +67,6 @@ class GameViewModel : ViewModel() {
         }
         return false
     }
-
 
     /*
     * Returns true if the current word count is less than MAX_NO_OF_WORDS.
