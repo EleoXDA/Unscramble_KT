afd8ff90e05c21e9bcf8e41d216a97dbc1ec539c
Elbay Malik <eleo.666@gmail.com>
2022-02-21 21:24:28 +0100

    Include method to bring new word

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,20 +4,38 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     private var score = 0
     private var currentWordCount = 0
 
-    private var _currentScrambledWord = "test"
+    private var _currentWordCount = 0
+
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
     override fun onCleared(){
         super.onCleared()
         Log.d("GameFragment", "GameViewModel is destroyed")
+    }
+    private fun getNextWord(){
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()}
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 }
